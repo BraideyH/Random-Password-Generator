c7595e513034009493c96ea1d8df732e1762c366
BraideyH <braideyhooper@gmail.com>
2023-10-05 23:09:45 -0500

    Finished README file and did one final clean sweep of excess code in js file for a cleaner look.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Password Generator Starter Code
+# Random Password Generator
+
+## Description
+
+
+This application creates a mathmatically random password while including/excluding various fields such as numbers, lowercase letters, uppercase letters, special characters. While giving the user those options, this app provides the user with a slidebar, giving them the choice of how long the password may be; with the minimum being 8 characters and the maximum being 127.
+
+
+## Installation
+
+To run this site, the user must first open any internet browser and supply it with the URL leading to the webpage.
+
+## Usage
+
+To effectively use this application, one must first specify the required fields desired and an appropriate password length. Once this step is completed, the user may click "Generate Password" button. Once completed, the desired password will appear in the middle box.
+
+
+![alt text](assets/images/PasswordGeneratorScreenshot.png)
+
+Live Site URl: https://braideyh.github.io/Random-Password-Generator/
+
+## Credits
+
+Started code was provided from https://github.com/coding-boot-camp/friendly-parakeet 
--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var symbols = "!@#$%^&*;";
 var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const passText = document.querySelector("#password")
-
 var checkboxes = document.querySelectorAll("input");
 let enabledSettings = []
 checkboxes.forEach(function(checkbox){
@@ -14,39 +13,29 @@ checkboxes.forEach(function(checkbox){
   .map(i => i.value)
 })
 })
-
 const value = document.querySelector("#value");
 const input = document.querySelector("#user-input");
 value.textContent = input.value;
 input.addEventListener("input", (event) => {
   value.textContent = event.target.value;
 });
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword)
-
 // Write password to the #password input
 function writePassword() {
   var numbersEL = document.getElementById("AllowNumbers");
   var includeNumbers = numbersEL.checked;
-
   var specialCharacterEL = document.getElementById("AllowSymbols");
   var includeSpecialCharacters = specialCharacterEL.checked;
-
   var lowercaseLettersEl = document.getElementById("AllowLowercaseLetters");
   var includelowercaseLetters = lowercaseLettersEl.checked;
-
   var uppercaseLettersEl = document.getElementById("AllowUpperCaseLetters");
   var includeuppercaseLetters = uppercaseLettersEl.checked;
-
   var passwordLengthEl = document.getElementById("user-input");
   var passValue = passwordLengthEl.value;
-  
   let pass = '';
-
   let passwordString = "";
-
   if(includeNumbers) {
     passwordString = passwordString + numbers
   }
@@ -59,7 +48,6 @@ function writePassword() {
   if(includeuppercaseLetters) {
     passwordString += uppercaseLetters
   }
-
   for (let i = 1; i <= passValue; i++) {
       let char = Math.floor(Math.random()
         * passwordString.length + 1);
@@ -67,17 +55,3 @@ function writePassword() {
   }
   passText.value = pass;
 }
-
-// themeSwitcher. addEventListener("click", function allowNumbers() {
-//   // If mode is dark, apply light background
-//   if (mode === "dark") {
-//     mode = "light";
-//     container.setAttribute("class", "light");
-//   }
-//   // If mode is light, apply dark background 
-//   else {
-//     mode = "dark";
-//     container.setAttribute("class", "dark");
-//   }
-// });
-
